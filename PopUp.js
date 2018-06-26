ede38e03aee5e9b33772823f26a207315c349c0a
Sarah Ronchetti <ronches@sea-ronches-m.ds.ad.adp.com>
2018-06-26 11:41:50 -0700

    CS_Demo

---
+++ PopUp.js
@@ -0,0 +1,191 @@
+
+
+//******PAYMENT OPTIONS*****//
+// Get the modal
+var PopUp1 = document.getElementById('myPopUp1');
+
+var whiteBG = document.getElementsByClassName('white_BG')[0];
+
+// Get the button that opens the modal
+var Open_button1 = document.getElementById("button1");
+
+// Get the <span> element that closes the modal
+var X1 = document.getElementsByClassName("closeWindow1")[0];
+
+// When the user clicks the button, open the modal
+Open_button1.onclick = function () {
+   PopUp1.style.display = "block";
+   whiteBG.style.display = "block";
+}
+
+
+// When the user clicks on <span> (x), close the modal
+X1.onclick = function ()
+{
+    PopUp1.style.display = "none";
+    whiteBG.style.display = "none";
+}
+
+
+//******VEHICLE PRICE*****//
+// Get the modal
+var PopUp2 = document.getElementById('myPopUp2');
+
+var whiteBG2 = document.getElementsByClassName('white_BG2')[0];
+
+// Get the button that opens the modal
+var Open_button2 = document.getElementById("button2");
+
+// Get the <span> element that closes the modal
+var X2 = document.getElementsByClassName("closeWindow2")[0];
+
+// When the user clicks the button, open the modal
+Open_button2.onclick = function () {
+    PopUp2.style.display = "block";
+    whiteBG2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+X2.onclick = function ()
+{
+        PopUp2.style.display = "none";
+        whiteBG2.style.display = "none";
+
+}
+
+
+
+//******INCENTIVES APPLIED*****//
+// Get the modal
+var PopUp3 = document.getElementById('myPopUp3');
+
+// Get the button that opens the modal
+var Open_button3 = document.getElementById("button3");
+
+// Get the <span> element that closes the modal
+var X3 = document.getElementsByClassName("closeWindow3")[0];
+
+// When the user clicks the button, open the modal
+Open_button3.onclick = function () {
+    PopUp3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+X3.onclick = function ()
+{
+    PopUp3.style.display = "none";
+ }
+
+
+//******TRADE IN VALUE*****//
+// Get the modal
+var PopUp4 = document.getElementById('myPopUp4');
+
+// Get the button that opens the modal
+var Open_button4 = document.getElementById("button4");
+
+// Get the <span> element that closes the modal
+var X4 = document.getElementsByClassName("closeWindow4")[0];
+
+// When the user clicks the button, open the modal
+Open_button4.onclick = function () {
+    PopUp4.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+X4.onclick = function ()
+{
+    PopUp4.style.display = "none";
+}
+
+
+//******TAXES AND FEES*****//
+// Get the modal
+var PopUp5 = document.getElementById('myPopUp5');
+
+// Get the button that opens the modal
+var Open_button5 = document.getElementById("button5");
+
+// Get the <span> element that closes the modal
+var X5 = document.getElementsByClassName("closeWindow5")[0];
+
+// When the user clicks the button, open the modal
+Open_button5.onclick = function ()
+{
+    PopUp5.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+X5.onclick = function ()
+{
+    PopUp5.style.display = "none";
+}
+
+//******CREDIT SCORE*****//
+// Get the modal
+var PopUp6 = document.getElementById('myPopUp6');
+
+// Get the button that opens the modal
+var Open_button6 = document.getElementById("button6");
+
+// Get the <span> element that closes the modal
+var X6 = document.getElementsByClassName("closeWindow6")[0];
+
+// When the user clicks the button, open the modal
+Open_button6.onclick = function ()
+{
+    PopUp6.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+X6.onclick = function ()
+{
+    PopUp6.style.display = "none";
+}
+
+
+//******PROTECT MY VEHICLE*****//
+// Get the modal
+var PopUp7 = document.getElementById('myPopUp7');
+
+// Get the button that opens the modal
+var Open_button7 = document.getElementById("button7");
+
+// Get the <span> element that closes the modal
+var X7 = document.getElementsByClassName("closeWindow7")[0];
+
+// When the user clicks the button, open the modal
+Open_button7.onclick = function ()
+{
+    PopUp7.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+X7.onclick = function ()
+{
+    PopUp7.style.display = "none";
+}
+
+
+
+
+//******REVIEW MY DEAL*****//
+// Get the modal
+//var PopUp8 = document.getElementById('myPopUp8');
+
+// Get the button that opens the modal
+//var Open_button8 = document.getElementById("button8");
+
+// Get the <span> element that closes the modal
+//var X8 = document.getElementsByClassName("closeWindow8")[0];
+
+// When the user clicks the button, open the modal
+//Open_button8.onclick = function () {
+   // PopUp8.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+//X8.onclick = function ()
+//{
+  //  PopUp8.style.display = "none";
+//}
